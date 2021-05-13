6ad6e2fd5665131b2613139747d2015e86f353a7
Mithunprb <61266869+Mithunprb@users.noreply.github.com>
2021-05-13 19:36:15 +0530

    Update Rock_Paper_Scissors.py
    reduced loc

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,5 +1,5 @@
 import random
 e, pos, lost, win, tie = input("Choose rock paper siccors or r,p,s:"), ["r", "p", "s", "rock", "paper", "siccors"],lambda: print("you lost")if e.lower() in pos else print("you lost"), lambda: print("you win")if e.lower(
-  ) in pos else print("you win"), lambda: print("TIE!")if e.lower() in pos else print("TIE!")
+  ) in pos else print("you win"), lambda: print("TIE!")if e.lower() in pos else print(e + ":  TIE!")
 func = random.choice([lost, win, tie])
 func()
